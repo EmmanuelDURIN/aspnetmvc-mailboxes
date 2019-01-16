33a275e9baec9adb6460757ca700b5f88a619e9e
Emmanuel DURIN <emmanuel.durin@gmail.com>
2019-01-16 16:08:39 +0100

    Génération du Create-Retrieve-Update-Delete de la mailbox par Scaffolding

--- MailBox/Controllers/MailBoxController.cs
+++ MailBox/Controllers/MailBoxController.cs
@@ -1,11 +1,23 @@
 ﻿using MailBoxManager.Models;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 
 namespace MailBoxManager.Controllers
 {
   public class MailBoxController : Controller
   {
+    private MailBoxContext db = new MailBoxContext();
+    public MailBoxController()
+    {
+
+    }
+    // GET: MailBox
+    public ActionResult Index()
+    {
+      return View(db.MailBoxes.ToList());
+    }
     // GET: MailBox
     public ActionResult Reference(string reference)
     {
@@ -16,7 +28,117 @@ namespace MailBoxManager.Controllers
              mb => mb.Reference.ToUpper() == reference.ToUpper());
       }
       // transmettre la mailbox en tant que modele de la vue
-      return View(model : mailbox);
+      // La vue par défaut qui est recherchée est la vue dont le nom correspond à l'action ...
+      return View(viewName:"Details", model: mailbox );
+    }
+    // GET: MailBox/Details/5
+    public ActionResult Details(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      MailBox mailBox = db.MailBoxes.Find(id);
+      if (mailBox == null)
+      {
+        return HttpNotFound();
+      }
+      return View(mailBox);
+    }
+
+    // GET: MailBox/Create
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    // POST: MailBox/Create
+    // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+    // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(
+      [Bind(Include = "Id,Reference,Name,Color,ImagePath,Height,Width,Depth")] MailBox mailBox)
+    {
+      if (ModelState.IsValid)
+      {
+        db.MailBoxes.Add(mailBox);
+        db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
+      return View(mailBox);
+    }
+
+    // GET: MailBox/Edit/5
+    public ActionResult Edit(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      MailBox mailBox = db.MailBoxes.Find(id);
+      if (mailBox == null)
+      {
+        return HttpNotFound();
+      }
+      return View(mailBox);
+    }
+
+    // POST: MailBox/Edit/5
+    // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+    // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit([Bind(Include = "Id,Reference,Name,Color,ImagePath,Height,Width,Depth")] MailBox mailBox)
+    {
+      if (ModelState.IsValid)
+      {
+        db.Entry(mailBox).State = EntityState.Modified;
+        db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+      return View(mailBox);
+    }
+
+    // GET: MailBox/Delete/5
+    public ActionResult Delete(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      MailBox mailBox = db.MailBoxes.Find(id);
+      if (mailBox == null)
+      {
+        return HttpNotFound();
+      }
+      return View(mailBox);
+    }
+
+    // POST: MailBox/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      MailBox mailBox = db.MailBoxes.Find(id);
+      db.MailBoxes.Remove(mailBox);
+      db.SaveChanges();
+      // pattern Post-Get
+      // L'action de suppression (création, édition,...) est dissociée du visionnage
+      // Post -> Supprime
+      // Get -> Voir la liste des MailBox 
+      // ( c'est uniquement cette deuxième requête qui est visée en cas de Refresh)
+      return RedirectToAction("Index");
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        db.Dispose();
+      }
+      base.Dispose(disposing);
     }
   }
 }
--- MailBox/Models/MailBoxContext.cs
+++ MailBox/Models/MailBoxContext.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Diagnostics;
-using System.Linq;
-using System.Web;
 
 namespace MailBoxManager.Models
 {
@@ -11,14 +7,19 @@ namespace MailBoxManager.Models
   public class MailBoxContext : DbContext
   {
     public MailBoxContext()
-            // faire le lien avec le ConnectionString 
-            //: base("name= MailBoxConnectionString")
+    // faire le lien avec le ConnectionString 
+    //: base("name= MailBoxConnectionString")
     // ou bien ne rien mettre et c’est une base localDB qui porte 
     // le nom pleinement qualifié de la classe de DbContext
     {
-      Debug.WriteLine(this.Database.Connection.ConnectionString);
+      //Debug.WriteLine(this.Database.Connection.ConnectionString);
       // On veut logger les requêtes SQL passées par EFx sur la DB
-      this.Database.Log = text => Debug.WriteLine(text);
+      this.Database.Log = text =>
+      {
+        Debug.WriteLine("******************************************************");
+        Debug.WriteLine(text);
+        Debug.WriteLine("******************************************************");
+      };
     }
     // pour chaque entité {classe <---> table} on expose un DbSet
     // qui permet de requêter
